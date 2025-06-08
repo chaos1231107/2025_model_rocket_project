ff330a9cceefad4e01d3ef17c5c654122eede4d7
Keonwoo Park <136959177+chaos1231107@users.noreply.github.com>
2025-06-08 15:45:46 +0900

    Update rt_lpf_test.py

--- control_system/rt_lpf_test.py
+++ control_system/rt_lpf_test.py
@@ -1,55 +1,50 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 def sma_initial(buffer, window):
     n = min(len(buffer), window)
     return sum(buffer) / n
 
-def lowpass_filter(alpha, window):
+def low_pass_filter(alpha=0.3, window=3, dt=0.1, total_steps=100):
     data_buffer = []
     lpf_buffer = []
 
-    for i in range(100):
+    plt.ion()
+    fig, ax = plt.subplots()
+    raw_line, = ax.plot([], [], label='Raw Data')
+    lpf_line, = ax.plot([], [], label='LPF')
+    ax.set_ylim(0, 100)
+    ax.set_xlim(0, total_steps * dt)
+    ax.set_xlabel('Time (s)')
+    ax.set_ylabel('Value')
+    ax.set_title('Real-Time LPF Processing')
+    ax.legend()
+
+    t = []
+
+    for i in range(total_steps):
+       
         new_data = np.random.randint(0, 100)
         data_buffer.append(new_data)
+        t.append(i * dt)
 
         if len(data_buffer) <= window:
             lpf_value = sma_initial(data_buffer, window)
         else:
             prev_lpf = lpf_buffer[-1]
-            lpf_value = alpha * prev_lpf + (1 - alpha) * new_data
+            lpf_value = (1-alpha) * prev_lpf + alpha * new_data
 
         lpf_buffer.append(lpf_value)
 
-    return data_buffer, lpf_buffer
+        raw_line.set_data(t, data_buffer)
+        lpf_line.set_data(t, lpf_buffer)
+        ax.set_xlim(max(0, t[i] - 10), t[i] + dt)
 
-alpha = 0.2
-window = 3
-dt = 0.1
-total_steps = 100
+        plt.pause(dt)
+        time.sleep(dt)  
 
-data_list, lpf_list = lowpass_filter(alpha, window)
+    plt.ioff()
+    plt.show()
 
-plt.ion()
-fig, ax = plt.subplots()
-raw_line, = ax.plot([], [], label='Raw Data')
-lpf_line, = ax.plot([], [], label='LPF')
-ax.set_ylim(0, 100)
-ax.set_xlim(0, total_steps * dt)
-ax.set_xlabel('Time (s)')
-ax.set_ylabel('Value')
-ax.set_title('Low-Pass Filter processing')
-ax.legend()
-
-t = []
-
-for i in range(total_steps):
-    t.append(i * dt)
-
-    raw_line.set_data(t[:i+1], data_list[:i+1])
-    lpf_line.set_data(t[:i+1], lpf_list[:i+1])
-    ax.set_xlim(max(0, t[i] - 10), t[i] + dt)
-    plt.pause(dt)
-
-plt.ioff()
-plt.show()
+low_pass_filter()
