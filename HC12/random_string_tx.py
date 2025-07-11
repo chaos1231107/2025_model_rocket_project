ef5d42cdf17a201a60306dffef8dc096092f890a
Keonwoo Park <136959177+chaos1231107@users.noreply.github.com>
2025-07-11 14:36:58 +0900

    Update random_string_tx.py

--- HC12/random_string_tx.py
+++ HC12/random_string_tx.py
@@ -3,18 +3,21 @@ import time
 import random
 import string
 
-def random_string(n=32):
+def random_string(n=100):
     pool = string.ascii_letters + string.digits
     return ''.join(random.choices(pool, k=n))
 
+total_sent = 0
+total_matched = 0
 
 ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
 time.sleep(2)
 print('Start pi Arduino Echo Test')
 
 while True:
-    msg = random_string(32)
+    msg = random_string(64)
     ser.write((msg + '\n').encode())
+    total_sent += 1
     print('TX:', msg)
 
     time.sleep(0.5)
@@ -22,6 +25,7 @@ while True:
     if ser.in_waiting:
         echo = ser.readline().decode(errors='ignore').strip()
         if echo == msg:
+            total_matched += 1
             print(f'RX : {msg}')
             print('matched')
         else:
@@ -29,5 +33,6 @@ while True:
     else:
         print('No Signal')
 
+    accuracy = (total_matched / total_sent) * 100
+    print(f'Accuracy : {accuracy:.2f}% ')
     time.sleep(0.5)
-
